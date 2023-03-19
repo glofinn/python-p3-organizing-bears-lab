bcab475e9fdd5ec50cf5e3df4f84197ee3753f6d
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-03-19 18:16:27 -0400

    tests pass

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1,'Mr. Chocolate', 3, 'M', 'Brown', 'angry', TRUE);
+INSERT INTO bears (id,name, age, sex, color, temperament, alive) VALUES (2,'Rowdy', 58, 'M', 'Black', 'angry', TRUE);
+INSERT INTO bears (id,name, age, sex, color, temperament, alive) VALUES (3,'Tabitha', 22, 'F', 'Green', 'angry', TRUE);
+INSERT INTO bears (id,name, age, sex, color, temperament, alive) VALUES (4,'Seargant Brown', 3, 'M', 'yellow', 'psychotic', TRUE);
+INSERT INTO bears (id,name, age, sex, color, temperament, alive) VALUES (5,'Melissa', 12, 'F', 'Blue', 'angry', TRUE);
+INSERT INTO bears (id,name, age, sex, color, temperament, alive) VALUES (6, NULL , 2, 'M', 'Green', 'high', TRUE);
+INSERT INTO bears (id,name, age, sex, color, temperament, alive) VALUES (7,'Wendy', 5, 'F', 'Orange', 'sleepy', TRUE);
+INSERT INTO bears (id,name, age, sex, color, temperament, alive) VALUES (8,'Joe', 6, 'F', 'Black', 'debt', TRUE);
